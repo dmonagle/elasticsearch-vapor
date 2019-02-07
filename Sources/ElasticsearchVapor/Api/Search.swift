2994a523ef497251204b222bab09329ea4be4ca7
David Monagle <david@monagle.com.au>
2019-02-07 14:39:18 +1100

    Added count API

--- Sources/ElasticsearchVapor/Api/Search.swift
+++ Sources/ElasticsearchVapor/Api/Search.swift
@@ -5,6 +5,10 @@
 //  Created by David Monagle on 25/9/18.
 //
 
+fileprivate struct ElasticsearchCountResponse : Decodable {
+    var count : UInt = 0
+}
+
 public extension ElasticsearchClient {
     public func search(index: ESIndexName, type: String? = nil, query: ESDictionary = [:], body: HTTPBody) throws -> Future<HTTPResponse> {
         let path : ESArray = [self.prefix(index)?.description, type, "_search"]
@@ -20,6 +24,14 @@ public extension ElasticsearchClient {
     public func searchAndAggregate<Model, AggType>(index: ESIndexName, type: String? = nil, query: ESDictionary = [:], body: HTTPBody) throws -> Future<ESSearchResponseWithAggs<Model, AggType>> {
         return try search(index: index, type: type, query: query, body: body).flatMap { httpResponse in
             return self.decodeAsync(body: httpResponse.body)
+        }
+    }
+
+    public func count(index: ESIndexName, type: String? = nil, query: ESDictionary = [:], body: HTTPBody) throws -> Future<UInt> {
+        let path : ESArray = [self.prefix(index)?.description, type, "_count"]
+        return request(method: .POST, path: path, query: query, requestBody: body).map { response in
+            let countResponse : ElasticsearchCountResponse = try self.decode(body: response.body)
+            return countResponse.count
         }
     }
 }
